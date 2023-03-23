44e71fc2e39a9afabfc383cb382f170727827551
dpeilitz <dpeilitz@kth.se>
2023-03-23 11:50:53 +0100

    till kapitel 8 av raytracer weekend, borde börja migrera till annat språk nu

--- C/main.cc
+++ C/main.cc
@@ -1,37 +1,63 @@
+#include "utility.h"
 #include "color.h"
+#include "ray.h"
 #include "vec3.h"
-
+#include "surface_list.h"
+#include "sphere.h"
+#include "surface.h"
+#include "camera.h"
 #include <iostream>
+
+color ray_color(const ray& r, const surface& world, int depth) {
+
+    // If we've exceeded the ray bounce limit, no more light is gathered.
+    if (depth <= 0)
+        return color(0,0,0);
+
+    hit_record rec;
+    if (world.hit(r, 0.001, std::numeric_limits<double>::infinity(), rec)) {
+        point3 target = rec.p + rec.normal + random_in_unit_sphere();
+        return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth-1);
+    }
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+}
 
 int main() {
 
     // Image
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
+    const int max_depth = 50;
 
-    const int image_width = 256;
-    const int image_height = 256;
+    // World
+    surface_list world;
+    world.add (make_shared<sphere>(point3(0,0,-1), 0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
+    
+    // Camera
+    camera cam;
 
     // Render
 
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
     for (int j = image_height-1; j >= 0; --j) {
-      std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-       /*
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 0.25;
-
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            color pixel_color(0, 0, 0);
+            for (int s = 0; s < samples_per_pixel; ++s) {
+                auto u = (i + random_double()) / (image_width-1);
+                auto v = (j + random_double()) / (image_height-1);
+                ray r = cam.get_ray(u, v);
+                pixel_color += ray_color(r, world, max_depth);
+            }
+            write_color(std::cout, pixel_color, samples_per_pixel);
         }
-       */ 
-      color pixel_color(double(i)/(image_width-1), double(j)/(image_height-1), 0.25);
-      write_color(std::cout, pixel_color);
     }
+
     std::cerr << "\nDone.\n";
-    }
 }
